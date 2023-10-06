7870b7a487323e347426dc3e55a511ebad875994
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-06 20:28:57 +0400

    Dervied State in useSelector function

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -14,11 +14,11 @@ function CarSearch() {
     return(
 
         <div class=" flex flex-row justify-between items-center">
-            <h3 class="mb-0"> My Cars </h3>
+            <h3 class="mt-5 mb-0 text-3xl font-semibold"> My Cars </h3>
             <div class="flex items-center">
-                <label class="mb-0 mr-6">Search</label>
+                <label class="mt-5 mb-0 mr-6 text-lg font-semibold">Search</label>
                 <input 
-                className="input"
+                class=" input mt-5 border border-gray-700"
                 value={searchTerm}
                 onChange={handleSearchTermChange} />
             </div>
