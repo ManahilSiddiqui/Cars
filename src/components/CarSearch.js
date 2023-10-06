c9afe999811e383dae21056fac9571fb4a52281e
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-06 19:25:53 +0400

    Added a Search input

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,7 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from '../store';
+
 function CarSearch() {
+    const dispatch = useDispatch();
+    const searchTerm = useSelector((state) => {
+        return state.cars.searchTerm;
+    });
+
+    const handleSearchTermChange = (event) => {
+        dispatch(changeSearchTerm(event.target.value));
+    };
+
     return(
 
-        <div>CarSearch</div>
+        <div class=" flex flex-row justify-between items-center">
+            <h3 class="mb-0"> My Cars </h3>
+            <div class="flex items-center">
+                <label class="mb-0 mr-6">Search</label>
+                <input 
+                className="input"
+                value={searchTerm}
+                onChange={handleSearchTermChange} />
+            </div>
+
+        </div>
     )
 }
 
